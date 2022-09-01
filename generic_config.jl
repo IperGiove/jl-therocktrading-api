c9d150683ba66c56e0cb39f51f40d4a4eb0459f4
ipergiove <ipergiove@gmail.com>
2022-09-01 17:11:25 +0200

    add currencies endpoint

--- generic_config.jl
+++ generic_config.jl
@@ -1,4 +1,6 @@
-using Dates
+using HTTP
+using JSON
+using SHA
 
 const ENDPOINT_STAGING = "https://api-staging.therocktrading.com"
 const ENDPOINT= "https://api.therocktrading.com"
@@ -34,15 +36,16 @@ end
 """
 Url creator
 """
-function url_creator(url_final::String) :: String
-    return url_base * url_final
+function url_creator(client::Client, url_final::String) :: String
+    return client.url_base * url_final
 end
 
 
 """
 Signature header
 """
-function signature_creator(message::String)
+function signature_creator(;client::Client, message::String)
+    enc = bytes2hex(hmac_sha256(Vector{UInt8}(client.API_SECRET), message))
 end
 
 
@@ -54,13 +57,31 @@ function header(;client::Client, url::String) :: Dict
     return Dict{String, String}(
         "Content-Type" => "application/json", 
         "X-TRT-KEY" => client.API,
-        #"X-TRT-SIGN" => signature_creator(nonce ** url),
+        "X-TRT-SIGN" => signature_creator(client=client, message=nonce*url),
         "X-TRT-NONCE" => nonce
     )
 end
 
+"""
+Make http request
+"""
+function requests_and_parse(http_method::String, url::String) :: Tuple{Dict{String, Any}, Int16}
+    r = HTTP.request(http_method, url, header=header(client=client, url=url))
+    return JSON.Parser.parse(String(r.body)), r.status
+end
 
-client = Client("API", "API_SECRET")
-#println(client)
 
-header(url="test", client=client)
+"""
+Get all currencies info.
+"""
+function currencies(client::Client) :: Tuple{Dict{String, Any}, Int16}
+    url = url_creator(client, "/currencies")
+    return requests_and_parse("GET", url)
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    client = Client("API", "API_SECRET")
+    
+    println(currencies(client))
+end
